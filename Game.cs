09876d7149c28468738d141993eef9ad3c91870b
ihorzxc <ihor.rudchenkozxc@gmail.com>
2022-12-25 13:43:29 +0200

    lab1
    lab1

---
+++ Game.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab1OOP;
+
+public class GameDTO
+{
+    public int index;
+    public GameAccount loser;
+    public GameAccount winner;
+    public int Rate;
+    public GameDTO(GameAccount loser, GameAccount winner, int rate, int index)
+    {
+        this.loser = loser;
+        this.winner = winner;
+        Rate = rate;
+        this.index = index;
+    }
+}
+
+public class Game
+{
+    private List<GameDTO> gamehistory = new List<GameDTO>();
+    private int index = 0;
+    public void play(GameAccount winner, GameAccount loser, int Rate)
+    {
+        if (Rate < 0)
+        {
+            throw new Exception("Rating can not be negative number");
+        }
+        else if (Rate + 1 <= loser.CurrentRating)
+        {
+            winner.WinGame(loser, Rate, index);
+            loser.LoseGame(winner, Rate, index);
+            GameDTO thisgame = new GameDTO(loser, winner, Rate, index);
+            gamehistory.Add(thisgame);
+            index += 1;
+        }
+    }
+
+    public void GetStats()
+    {
+        for (int i = 0; i < gamehistory.Count; i++)
+        {
+            Console.WriteLine($"Game {i}\n{gamehistory[i].winner.UserName} (+{gamehistory[i].Rate} points) won\n{gamehistory[i].loser.UserName} (-{gamehistory[i].Rate} points) lose");
+        }
+    }
+}
---
+++ GameAccount.cs
@@ -0,0 +1,56 @@
+﻿namespace Lab1OOP;
+
+public class GameAccount
+{
+    public string UserName = "None";
+    public int CurrentRating;
+    private int GamesCount = 0;
+    private List<GameDTO> opponenthistory = new List<GameDTO>();
+
+    public GameAccount(string UserName)
+    {
+        this.UserName = UserName;
+        CurrentRating = 100;
+    }
+    
+    public GameAccount()
+    {
+        CurrentRating = 100;
+        
+    }
+
+    public void WinGame(GameAccount opponent, int Rating, int index)
+    {
+        CurrentRating += Rating;
+        GameDTO game = new GameDTO(opponent, this, Rating, index);
+        opponenthistory.Add(game);
+    }
+    
+    public void LoseGame(GameAccount opponent, int Rating, int index)
+    {
+        CurrentRating -= Rating;
+        GameDTO game = new GameDTO(this, opponent, Rating, index);
+        opponenthistory.Add(game);
+    }
+
+    public void GetStats()
+    {
+        Console.WriteLine($"History of {UserName}");
+        for (int i = 0; i < opponenthistory.Count; i++)
+        {
+            Console.WriteLine($"Game {opponenthistory[i].index}");
+            if (opponenthistory[i].winner == this)
+            {
+                Console.WriteLine($"\twon against {opponenthistory[i].loser.UserName}");
+            }
+            else
+            {
+                Console.WriteLine($"\tlose against {opponenthistory[i].winner.UserName}");
+            }
+            
+            
+            Console.WriteLine($"\tRate {opponenthistory[i].Rate}");
+        }
+        Console.WriteLine($"Now {UserName} has {CurrentRating} points\n");
+    }
+}
